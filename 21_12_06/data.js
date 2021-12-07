d6deafb89a538667f4506d0c6b17d6334a688593
Villain_uni <44321712+SeongYoonMin@users.noreply.github.com>
2021-12-07 09:26:16 +0900

    Add files via upload

---
+++ 21_12_06/data.js
@@ -0,0 +1,67 @@
+let data = [
+    {
+      "_id": "61ad9e15e05422d6ab978aed",
+      "index": 0,
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Santiago Rios",
+      "gender": "male",
+      "company": "PROXSOFT",
+      "email": "santiagorios@proxsoft.com",
+      "phone": "+1 (846) 586-2452",
+      "address": "397 Tech Place, Retsof, Pennsylvania, 4749"
+    },
+    {
+      "_id": "61ad9e15e46357d43d22c5aa",
+      "index": 1,
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Winifred Spencer",
+      "gender": "female",
+      "company": "JAMNATION",
+      "email": "winifredspencer@jamnation.com",
+      "phone": "+1 (841) 429-3926",
+      "address": "453 Willmohr Street, Ladera, Alabama, 3605"
+    },
+    {
+      "_id": "61ad9e15536fda87ed456484",
+      "index": 2,
+      "picture": "http://placehold.it/32x32",
+      "age": 28,
+      "eyeColor": "brown",
+      "name": "Chasity Horne",
+      "gender": "female",
+      "company": "NETROPIC",
+      "email": "chasityhorne@netropic.com",
+      "phone": "+1 (849) 424-3977",
+      "address": "434 Wolcott Street, Concho, Louisiana, 887"
+    },
+    {
+      "_id": "61ad9e15b5a8d5839871636b",
+      "index": 3,
+      "picture": "http://placehold.it/32x32",
+      "age": 22,
+      "eyeColor": "blue",
+      "name": "Shanna Reynolds",
+      "gender": "female",
+      "company": "FUTURIS",
+      "email": "shannareynolds@futuris.com",
+      "phone": "+1 (979) 556-3252",
+      "address": "101 Kane Place, Tuttle, Massachusetts, 4934"
+    },
+    {
+      "_id": "61ad9e1589dc476a7f23dd4e",
+      "index": 4,
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Oliver Warren",
+      "gender": "male",
+      "company": "ZENSOR",
+      "email": "oliverwarren@zensor.com",
+      "phone": "+1 (945) 411-2658",
+      "address": "702 Duffield Street, Roderfield, Utah, 5702"
+    }
+  ]
